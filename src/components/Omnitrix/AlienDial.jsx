7b04c82150c075bc391ec171170490d45d42fbcc
Deo Cherisme <deolenscherisme1000@gmail.com>
2026-01-24 23:10:21 -0500

    Add alien selection with state and display info card

--- src/components/Omnitrix/AlienDial.jsx
+++ src/components/Omnitrix/AlienDial.jsx
@@ -1,11 +1,30 @@
-function AlienDial({ aliens }) {
-    return(
-        <div>
-            {aliens.map((alien) => (
-                <button key={alien.id}>{alien.name}</button>
-            ))}
-        </div>
-    )
+// This component renders a list of buttons for each alien
+// It receives an array of alien objects as a prop called `aliens`
+function AlienDial({ aliens , onAlienSelect }) {
+
+  return (
+    // Wrapper div for all alien buttons
+    <div>
+
+      {/*
+        We use .map() to loop through the aliens array
+        For each alien, we create a button
+      */}
+      {aliens.map((alien) => (
+
+        // Each button needs a unique `key` so React can track list items efficiently
+        // We use alien.id because it should be unique
+        <button key={alien.id} onClick={() => onAlienSelect(alien)}>
+
+          {/* Display the alien's name on the button */}
+          {alien.name}
+
+        </button>
+      ))}
+
+    </div>
+  );
 }
 
+// Export the component so it can be used in other files
 export default AlienDial;
--- src/components/Omnitrix/Omnitrix.jsx
+++ src/components/Omnitrix/Omnitrix.jsx
@@ -1,11 +1,40 @@
+import { useState } from "react";
+// Import the AlienDial component so we can use it inside Omnitrix
 import AlienDial from "./AlienDial";
+import AlienInfoCard from "../AlienInfoCard/AlienInfoCard";
+// Omnitrix is a parent component
+// It receives an array of aliens as a prop
 
-function Omnitrix({aliens }) {
-  return (
-    <div>
-        <AlienDial aliens={aliens} />
-    </div>
-    )
+
+
+function Omnitrix({ aliens  }) {
+
+const [selectedAlien, setSelectedAlien] = useState(null);
+
+function handleAlienSelect(alien) {
+    console.log("Selected:", alien.name);
+    setSelectedAlien(alien);
 }
 
+  return (
+    // Wrapper div for the Omnitrix UI
+    <div>
+
+      {/*
+        We pass the `aliens` prop down to AlienDial
+        This is called "props drilling" (parent → child)
+      */}
+      <AlienDial 
+      aliens={aliens} 
+      onAlienSelect={handleAlienSelect} />
+       {!selectedAlien && <p>No alien selected.</p>}
+       {selectedAlien && <p>Selected alien: {selectedAlien.name}</p>}
+
+       
+        {selectedAlien && <AlienInfoCard alien={selectedAlien} />}
+    </div>
+  );
+}
+
+// Export so Omnitrix can be used in App or other components
 export default Omnitrix;
